01c4fccb5899ffda31801d480c677abcb87a80ad
sometime1992 <sometime1992@naver.com>
2024-08-19 10:29:23 +0900

    20240819

--- c_javascript/z_HTML_CSS_JS/c_weather/main.js
+++ c_javascript/z_HTML_CSS_JS/c_weather/main.js
@@ -2,20 +2,33 @@
 
 const city = "Seoul";
 // >> input창에서 입력받을 경우 capitalize 사용
-const apikey = "b3d2a174bd0bf979992c01994dd21eea";
+// EX) London, Tokyo 등
+
+const apikey = "b3d2a174bd0bf979992c01994dd21eea"; // API 사이트에서 제공하는 개인 API_KEY
 const lang = "kr";
 
 const api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&lang=${lang}&units=metric`;
 
+// >> 제공된 API를 사용하여 지역의 이름을 바탕으로
+// >> 해당 날씨에 대한 데이터를 lang(언어)속성을 지정하여 원하는 언어로 호출
+// >> &units=metric: 시간이 지남에 따라 변화하는 데이터를 의미
+
 async function getWeatherData() {
     try {
         const response = await fetch(api);
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
+        // API로 가져오는 데이터는 JSON문자열 형식
+        // >> 객체로 변환
         const data = await response.json();
+
         console.log(data);
-        console.log(data.main.temp);
+        console.log(data.main.temp); // 35.19
+        console.log(data.coord.lon); // 126.9778
+        
     } catch (error) {
         console.error('Error:', error);
     }
@@ -24,6 +37,8 @@ async function getWeatherData() {
 getWeatherData();
 
 /*
+# data 객체 내부의 속성
+
 ! coord: 좌표 정보
 
 lon: 경도 (126.9778)
@@ -85,12 +100,12 @@ sunset: 일몰 시간 (Unix 타임스탬프, 1723544872)
 일출과 일몰 시간은 Unix 타임스탬프로 출력 - 한국 표준시(GMT+9) 기준
 
 * ===== main: 날씨의 주 상태 ('Clear', 맑음) ===== *
-Clear: 맑음
-Clouds: 구름 많음
-Rain: 비
+Clear: 맑음*
+Clouds: 구름 많음*
+Rain: 비*
 Drizzle: 이슬비
 Thunderstorm: 뇌우 (천둥번개)
-Snow: 눈
+Snow: 눈*
 Mist: 안개
 Smoke: 연기
 Haze: 실안개
@@ -101,3 +116,32 @@ Ash: 화산재
 Squall: 돌풍
 Tornado: 토네이도
 */
+const days = document.getElementById('day');
+const currentDate = new Date();
+
+const option = { year: "numeric", month: "long", day: "numeric", weekday: "long" };
+const nowDay = currentDate.toLocaleDateString('en-US', option);
+days.textContent = nowDay;
+
+
+
+
+const button = document.querySelector('toggle-slider');
+const overlay = document.querySelector('.overlay');
+
+button.addEventListener('click', () => {
+    
+    if (button.classList.contains('dark')) {
+    
+    button.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+
+    button.classList.remove('dark');
+    } else {
+    
+    button.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    
+    button.classList.add('dark');
+    }
+});
---
+++ d_typescript/b_환경설정/a_settings.ts
@@ -0,0 +1,21 @@
+// a_settings.ts
+
+/*
+# 타입스크립트 환경 설정
+
+! 1. Node.js 설치
+
+- Node.js 설치 확인 명령어
+: cmd (명령프롬프트 창)
+  node -v
+  npm -v
+
+? npm 기본 명령어
+
+1. npm init
+  : 새로운 Node.js 프로젝트를 시작
+    >> package.json 파일을 생성
+  : -y 옵션 추가
+    >> 질문없이 기본값으로 package.json 파일을 생성
+2. 
+*/
